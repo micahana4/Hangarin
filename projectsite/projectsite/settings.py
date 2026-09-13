a46a267857fc52745a51c88d62f58b96e0bd80f0
Milcahana Montemayor <Mica@Mica.local>
2026-09-13 17:22:19 +0800

    modify allowed host

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-03$hav%^*%o93k_$wtu-%qc0p4ozb9=p+2n%74rc=_&*hw%#o+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['HangarinToDoApp.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -119,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
